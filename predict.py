0c91f0f4a8241d63462cc78da92d1652cb45deba
victen <vikramnimmakuri@gmail.com>
2022-03-09 03:35:15 -0800

    first commit

---
+++ predict.py
@@ -0,0 +1,17 @@
+import tesserocr
+from PIL import Image
+
+class ocr:
+    def __init__(self, filename):
+        self.filename = filename
+
+
+
+    def getPrediction(self):
+        image = Image.open(self.filename)
+        lines = tesserocr.image_to_text(image)  # print ocr text from image
+        #for line in lines.split("\r"):
+         #   print(line)
+        return lines
+
+
